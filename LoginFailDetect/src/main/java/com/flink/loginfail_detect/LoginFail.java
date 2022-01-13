7fdf04f5256a0a6caef74053c5f9b49bdc4170c1
imkew <imwangke@163.com>
2022-01-13 09:11:16 +0800

    Initial commit

---
+++ LoginFailDetect/src/main/java/com/flink/loginfail_detect/LoginFail.java
@@ -0,0 +1,8 @@
+package com.flink.loginfail_detect;
+
+/**
+ * @Author: kewang
+ * @Date: 2022/1/12 18:58
+ */
+public class LoginFail {
+}
---
+++ LoginFailDetect/src/main/java/com/flink/loginfail_detect/beans/LoginEvent.java
@@ -0,0 +1,8 @@
+package com.flink.loginfail_detect.beans;
+
+/**
+ * @Author: kewang
+ * @Date: 2022/1/12 18:59
+ */
+public class LoginEvent {
+}
